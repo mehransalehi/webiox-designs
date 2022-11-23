c3a6f0ce0e287fb19fb98df91987b03edb4f6d37
mehran salehi <msalehi090@gmail.com>
2022-11-23 11:40:51 +0330

    New index design

---
+++ js/index.js
@@ -0,0 +1,10 @@
+jQuery( document ).ready(function() {
+
+                        WebFont.load({
+                            
+                            
+                        });
+
+                    initAsoopAnime();
+                    });
+                    
